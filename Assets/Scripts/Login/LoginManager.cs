ca8de30ba18ca4cc28125d36986ae002483b19f3
lim-james <jameslimbj@gmail.com>
2020-05-30 16:55:44 +0800

    [Add] Initial photon setup

--- Assets/Scripts/Login/LoginManager.cs
+++ Assets/Scripts/Login/LoginManager.cs
@@ -2,105 +2,135 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LoginManager : MonoBehaviour
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+
+namespace Photon.Pun
 {
-    [SerializeField]
-    private InputField usernameField;
-    [SerializeField]
-    private InputField passwordField;
-    [SerializeField]
-    private Button enterButton;
-    [SerializeField]
-    private Text messageLabel;
-    [SerializeField]
-    private float segueDelay;
-
-    private float et;
-    private bool success;
-
-    private void SetError(string message)
+    public class LoginManager : MonoBehaviourPun
     {
-        messageLabel.color = new Color(1, 0, 0);
-        messageLabel.text = message;
+        [SerializeField]
+        private InputField usernameField;
+        [SerializeField]
+        private InputField passwordField;
+        [SerializeField]
+        private Button enterButton;
+        [SerializeField]
+        private Text messageLabel;
+        [SerializeField]
+        private float segueDelay;
+
+        private float et;
+        private bool success;
+
+        private void SetError(string message)
+        {
+            messageLabel.color = new Color(1, 0, 0);
+            messageLabel.text = message;
+        }
+
+        private void SetSuccess(string message)
+        {
+            messageLabel.color = new Color(1, 1, 1);
+            messageLabel.text = message;
+        }
+
+        private bool UserExist(string username)
+        {
+            // TODO - Check if user exists
+            return username == "root";
+        }
+
+        private void Awake()
+        {
+            PhotonNetwork.NetworkingClient.EventReceived += OnEvent;
+        }
+
+        private void OnDestroy()
+        {
+            PhotonNetwork.NetworkingClient.EventReceived -= OnEvent;
+        }
+
+        private void Start()
+        {
+            et = 0.0f;
+            success = false;
+        }
+
+        private void Update()
+        {
+            if (success && (et += Time.deltaTime) > segueDelay)
+                SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
+        }
+
+        private void OnEvent(EventData obj)
+        {
+            if (obj.Code == 1)
+            {
+                string data = (string)obj.CustomData;
+                Debug.Log(string.Format("Message from Server: {0}", data));
+            }
+        }
+
+        public void EnterHandler()
+        {
+            Debug.Log("Raised!!");
+            byte evCode = 1;
+            string name = "Jmaes";
+            PhotonNetwork.RaiseEvent(evCode, name, RaiseEventOptions.Default, SendOptions.SendReliable);
+
+            enterButton.enabled = false;
+
+            string username = usernameField.text;
+            string password = passwordField.text;
+
+            if (username.Length == 0)
+            {
+                SetError("Please enter username");
+                enterButton.enabled = true;
+                return;
+            }
+
+            if (username.Length > 20)
+            {
+                SetError("Choose a shorter username");
+                enterButton.enabled = true;
+                return;
+            }
+
+            if (password.Length < 8)
+            {
+                SetError("Password too short. (Length between 8 and 20)");
+                enterButton.enabled = true;
+                return;
+            }
+
+            if (password.Length > 20)
+            {
+                SetError("Password too long. (Length between 8 and 20)");
+                enterButton.enabled = true;
+                return;
+            }
+
+            string error;
+
+            if (UserExist(username))
+            {
+                success = AccountManager.Instance().Login(username, password, out error);
+                if (success) SetSuccess("Welcome back " + username);
+            }
+            else
+            {
+                success = AccountManager.Instance().Signup(username, password, out error);
+                if (success) SetSuccess("Account created. Welcome " + username);
+            }
+
+            if (!success)
+            {
+                enterButton.enabled = true;
+                SetError(error);
+            }
+        }
+
     }
-
-    private void SetSuccess(string message)
-    {
-        messageLabel.color = new Color(1, 1, 1);
-        messageLabel.text = message;
-    }
-
-    private bool UserExist(string username)
-    {
-        // TODO - Check if user exists
-        return username == "root";
-    }
-
-    private void Start()
-    {
-        et = 0.0f;
-        success = false;
-    }
-
-    private void Update()
-    {
-        if (success && (et += Time.deltaTime) > segueDelay)
-            SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
-    }
-
-    public void EnterHandler()
-    {
-        enterButton.enabled = false;
-
-        string username = usernameField.text;
-        string password = passwordField.text;
-
-        if (username.Length == 0)
-        {
-            SetError("Please enter username");
-            enterButton.enabled = true;
-            return;
-        }
-
-        if (username.Length > 20)
-        {
-            SetError("Choose a shorter username");
-            enterButton.enabled = true;
-            return;
-        }
-
-        if (password.Length < 8)
-        {
-            SetError("Password too short. (Length between 8 and 20)");
-            enterButton.enabled = true;
-            return;
-        }
-
-        if (password.Length > 20)
-        {
-            SetError("Password too long. (Length between 8 and 20)");
-            enterButton.enabled = true;
-            return;
-        }
-
-        string error;
-
-        if (UserExist(username))
-        {
-            success = AccountManager.Instance().Login(username, password, out error);
-            if (success) SetSuccess("Welcome back " + username);
-        }
-        else
-        {
-            success = AccountManager.Instance().Signup(username, password, out error);
-            if (success) SetSuccess("Account created. Welcome " + username);
-        }
-
-        if (!success)
-        {
-            enterButton.enabled = true;
-            SetError(error);
-        }
-    }
-
 }
---
+++ Assets/Scripts/Login/NetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+
+namespace Photon.Pun
+{
+    public class NetworkManager : MonoBehaviourPunCallbacks
+    {
+
+        private void Awake()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("On connected");
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        //public override void OnJoinedLobby()
+        //{
+        //    Debug.Log("On joined");
+        //    PhotonNetwork.JoinOrCreateRoom("testRoom");
+        //}
+    }
+}
